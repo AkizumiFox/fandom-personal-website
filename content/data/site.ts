04bb75f207a18246c715eae49cf7d4e6da9dad29
AkizumiFox <akizumi.fox@gmail.com>
2026-03-29 15:06:38 +0800

    update: xxx

--- content/data/site.ts
+++ content/data/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: "秋墨",
-  description: "A personal website for writing, research, and fandom.",
-  author: "Aaron Lin"
+  title: "秋墨的網站",
+  description: "一隻黑色狐狸的個狐網站，包含我是誰、我委託圖片跟照片、文字創作等等的內容。",
+  author: "秋墨"
 } as const;
